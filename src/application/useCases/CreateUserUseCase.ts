1480600bb9e246fac243430a11aec593bc24937b
junio-gon <junio-gon@hotmail.com>
2025-09-06 15:02:22 -0300

    initial commit
    Signed-off-by: junio-gon <junio-gon@hotmail.com>

---
+++ src/application/useCases/CreateUserUseCase.ts
@@ -0,0 +1,18 @@
+import { IUserRepository } from "@domain/repositories/IUserRepository";
+import { User } from "@domain/entities/User";
+
+export class CreateUserUseCase {
+    constructor(private userRepository: IUserRepository){}
+
+    async execute(name: string, email: string): Promise<User> {
+        const existingUser = await this.userRepository.findByEmail(email);
+
+        if (existingUser) {
+            throw new Error("Já existe um usuário registrado com este e-mail.");
+        }
+
+        const user = new User(name, email);
+
+        return await this.userRepository.save(user);
+    }
+}
